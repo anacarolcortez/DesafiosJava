3bb3580eb34452e5de35ddbf1071aad9e067c092
Ana Carol Cortez <56210395+anacarolcortez@users.noreply.github.com>
2020-04-28 20:34:29 -0300

    Desafio Calculador de Classes Módulo 3

--- CalculadorDeClasses.java
+++ CalculadorDeClasses.java
@@ -4,56 +4,53 @@ import com.challenge.interfaces.Calculavel;
 import com.challenge.annotation.Somar;
 import com.challenge.annotation.Subtrair;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 
 
 public class CalculadorDeClasses implements Calculavel {
 
-
-    public ArrayList<Object> acumuladorBigDecimal(Object classe) {
-        ArrayList<Object> todosBigDecimal = new ArrayList<>();
+    public BigDecimal acumuladorSoma(Object classe, Class annotation) {
+        BigDecimal somaBigDecimal = BigDecimal.ZERO;
         try {
-            Field[] parametros = Object.class.getDeclaredFields();
-            for (Field parametro: parametros){
-                if (parametro.getType().isAssignableFrom(BigDecimal.class)){
-                    todosBigDecimal.add(parametro);
+            Field[] parametros = Object.class.getFields();
+            for (Field parametro : parametros) {
+                parametro.setAccessible(true);
+                if (parametro.isAnnotationPresent(Somar.class) && parametro.getType().isAssignableFrom(BigDecimal.class)) {
+                    somaBigDecimal = somaBigDecimal.add(new BigDecimal(parametro.getDouble(BigDecimal.class)));
                 }
             }
-        } catch (Throwable e) {
-            System.out.println("Erro ao acessar atributos da classe");
-        }
-        return todosBigDecimal;
-    }
-
-    private BigDecimal fazerCalculoBase(Object classe, Class annotation) {
-        BigDecimal somaBigDecimal = new BigDecimal(0);
-        try {
-            Annotation[] annotations = Object.class.getDeclaredAnnotations();
-            if (annotations.length == 0) {
-                somaBigDecimal =  BigDecimal.ZERO;
-            } else {
-                ArrayList<Object> todosBigDecimal = acumuladorBigDecimal(classe);
-                for (Object bdec : todosBigDecimal) {
-                    somaBigDecimal = somaBigDecimal.add((BigDecimal) bdec);
-                }
-            }
-        } catch (Throwable e) {
+        }catch (Throwable e) {
             System.out.println("Erro ao acessar atributos da classe");
         }
         return somaBigDecimal;
     }
 
+    public BigDecimal acumuladorSubtracao(Object classe, Class annotation) {
+        BigDecimal somaBigDecimal = BigDecimal.ZERO;
+        try {
+            Field[] parametros = Object.class.getFields();
+            for (Field parametro : parametros) {
+                parametro.setAccessible(true);
+                if (parametro.isAnnotationPresent(Subtrair.class) && parametro.getType().isAssignableFrom(BigDecimal.class)) {
+                    somaBigDecimal = somaBigDecimal.add(new BigDecimal(parametro.getDouble(BigDecimal.class)));
+                }
+            }
+        }catch (Throwable e) {
+            System.out.println("Erro ao acessar atributos da classe");
+        }
+        return somaBigDecimal;
+    }
+
+
     @Override
     public BigDecimal somar(Object classe) throws IllegalAccessException {
-        return fazerCalculoBase(classe, Somar.class);
+        return acumuladorSoma(classe, Somar.class);
     }
 
     @Override
     public BigDecimal subtrair(Object classe) throws IllegalAccessException {
-        return fazerCalculoBase(classe, Subtrair.class);
+        return acumuladorSubtracao(classe, Subtrair.class);
     }
 
     @Override
