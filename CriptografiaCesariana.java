7eaf15037ba90e2863caec8ae9ecf3323ff6b101
Ana Carol Cortez <56210395+anacarolcortez@users.noreply.github.com>
2020-05-01 15:54:30 -0300

    Desafio Criptografia Cesariana Módulo 1

--- CriptografiaCesariana.java
+++ CriptografiaCesariana.java
@@ -1,54 +1,41 @@
+package challenge;
+
 import java.util.Objects;
 
-public class CriptografiaCesariana {
+public class CriptografiaCesariana implements Criptografia {
     final int CHAVE = 3;
-    
+
+    @Override
     public String criptografar(String texto) {
         pegaErro(texto);
         char [] fraseOriginal = texto.toLowerCase().toCharArray();
         char [] fraseNova = new char[fraseOriginal.length];
 
         for(int i : fraseOriginal) {
-            if (fraseOriginal[i] >= 97 && fraseOriginal[i] < 120) {
-                fraseNova[i] = (char) (fraseOriginal[i]+CHAVE);
-            }else if (fraseOriginal[i] >= 120 && fraseOriginal[i] <= 122 ) {
-                fraseNova[i] = (char) (fraseOriginal[i]-20-CHAVE);
-            } else {
-                fraseNova[i] = (char) (fraseOriginal[i]);
-            }
-
+            fraseNova[i] = (char) (fraseOriginal[i]);
+            if (fraseOriginal[i] >= 97 && fraseOriginal[i] < 120) { fraseNova[i] = (char) (fraseOriginal[i]+CHAVE); }
+            if (fraseOriginal[i] >= 120 && fraseOriginal[i] <= 122 ) { fraseNova[i] = (char) (fraseOriginal[i]-20-CHAVE); }
         }
         return new String (fraseNova);
     }
-    
+
+    @Override
     public String descriptografar(String texto) {
         pegaErro(texto);
         char [] fraseOriginal = texto.toLowerCase().toCharArray();
         char [] fraseNova = new char[fraseOriginal.length];
 
         for(int i : fraseOriginal) {
-            if (fraseOriginal[i] > 99 && fraseOriginal[i] <= 122) {
-                fraseNova[i] = (char) (fraseOriginal[i]-CHAVE);
-            }else if (fraseOriginal[i] >= 97 && fraseOriginal[i] <= 99 ) {
-                fraseNova[i] = (char) (fraseOriginal[i]+20+CHAVE);
-            } else {
-                fraseNova[i] = (char) (fraseOriginal[i]);
-            }
-
+            fraseNova[i] = (char) (fraseOriginal[i]);
+            if (fraseOriginal[i] > 99 && fraseOriginal[i] <= 122) { fraseNova[i] = (char) (fraseOriginal[i]-CHAVE); }
+            if (fraseOriginal[i] >= 97 && fraseOriginal[i] <= 99 ) {fraseNova[i] = (char) (fraseOriginal[i]+20+CHAVE); }
         }
-
         return new String (fraseNova);
     }
 
     private void pegaErro(String text) {
-        if (Objects.isNull(text)) {
-            throw new NullPointerException();
-        }
-
-        if (text.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
+        if (Objects.isNull(text)) throw new NullPointerException();
+        if (text.isEmpty()) throw new IllegalArgumentException();
     }
-
 
 }
