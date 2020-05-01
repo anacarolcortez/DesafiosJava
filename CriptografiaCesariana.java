59f491907517a4b39d24e33e382850ccd09ddabf
Ana Carol Cortez <56210395+anacarolcortez@users.noreply.github.com>
2020-05-01 15:42:27 -0300

    Desafio Criptografia Cesariana Módulo 1

--- CriptografiaCesariana.java
+++ CriptografiaCesariana.java
@@ -1,20 +1,17 @@
-package challenge;
-
 import java.util.Objects;
 
-public class CriptografiaCesariana implements Criptografia {
+public class CriptografiaCesariana {
     final int CHAVE = 3;
-
-    @Override
+    
     public String criptografar(String texto) {
         pegaErro(texto);
         char [] fraseOriginal = texto.toLowerCase().toCharArray();
         char [] fraseNova = new char[fraseOriginal.length];
 
-        for(int i = 0; i < fraseOriginal.length; i++) {
+        for(int i : fraseOriginal) {
             if (fraseOriginal[i] >= 97 && fraseOriginal[i] < 120) {
                 fraseNova[i] = (char) (fraseOriginal[i]+CHAVE);
-            }else if (fraseOriginal[i] >= 97 && fraseOriginal[i] <= 122 ) {
+            }else if (fraseOriginal[i] >= 120 && fraseOriginal[i] <= 122 ) {
                 fraseNova[i] = (char) (fraseOriginal[i]-20-CHAVE);
             } else {
                 fraseNova[i] = (char) (fraseOriginal[i]);
@@ -23,17 +20,16 @@ public class CriptografiaCesariana implements Criptografia {
         }
         return new String (fraseNova);
     }
-
-    @Override
+    
     public String descriptografar(String texto) {
         pegaErro(texto);
         char [] fraseOriginal = texto.toLowerCase().toCharArray();
         char [] fraseNova = new char[fraseOriginal.length];
 
-        for(int i = 0; i < fraseOriginal.length; i++) {
+        for(int i : fraseOriginal) {
             if (fraseOriginal[i] > 99 && fraseOriginal[i] <= 122) {
                 fraseNova[i] = (char) (fraseOriginal[i]-CHAVE);
-            }else if (fraseOriginal[i] >= 97 && fraseOriginal[i] <= 122 ) {
+            }else if (fraseOriginal[i] >= 97 && fraseOriginal[i] <= 99 ) {
                 fraseNova[i] = (char) (fraseOriginal[i]+20+CHAVE);
             } else {
                 fraseNova[i] = (char) (fraseOriginal[i]);
@@ -56,4 +52,3 @@ public class CriptografiaCesariana implements Criptografia {
 
 
 }
-
